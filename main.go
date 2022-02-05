914ae910cfe4bbd4b2a4d9f273e5d1ec36d10d3f
Yuniru Yuni <yuniruyuni@gmail.com>
2022-02-05 22:49:48 +0900

    feat: output same as input source code

--- main.go
+++ main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 const content = `
-@.str = private unnamed_addr constant [5 x i8] c"test\00", align 1
+@.str = private unnamed_addr constant [%d x i8] c"%s\00", align 1
 
 define dso_local i32 @main() #0 {
-  %1 = alloca i32, align 4
-  store i32 0, i32* %1, align 4
-  %2 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([5 x i8], [5 x i8]* @.str, i64 0, i64 0))
+  %%1 = alloca i32, align 4
+  store i32 0, i32* %%1, align 4
+  %%2 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([%d x i8], [%d x i8]* @.str, i64 0, i64 0))
   ret i32 0
 }
 
 declare i32 @printf(i8*, ...) #1`
 
+var sc = bufio.NewScanner(os.Stdin)
+
+type LLFile struct {
+	Word string
+}
+
+const (
+	nullCharSize = 1
+)
+
+func (ll *LLFile) Generate() string {
+	// it needs to calculate byte size.
+	l := len(ll.Word) + nullCharSize
+	w := ll.Word
+	return fmt.Sprintf(content, l, w, l, l)
+}
+
+func outputLL(word string) {
+	ll := LLFile{Word: word}
+	fmt.Print(ll.Generate())
+}
+
 func main() {
-	fmt.Println(content)
+	sc.Scan()
+	word := sc.Text()
+	outputLL(word)
 }
--- test.sh
+++ test.sh
@@ -19,4 +19,6 @@ test() {
     fi
 }
 
-test "" "test"
+test "" ""
+test "test" "test"
+test "日本語" "日本語"
